45035a9e3801e1f748f825a03a14196cbc467b96
aqib123-jpg <aqibk5867@gmail.com>
2024-12-21 17:45:58 +0500

    Updated server.js to modify backend functionalit555y

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.get('/api/users', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
